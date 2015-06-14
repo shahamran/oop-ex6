eb60f1149b0c7fee894a11e42729b5bea57dd70b
Ran Shaham <shahamran91@gmail.com>
2015-06-14 08:53:22 +0300

    Regex fun!

---
+++ src/Test.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Test {
+	private static final String NAME_STR_REGEX = "(?:(?:[A-Za-z]\\w*)|(?:_\\w+))",
+			TYPE_STR_REGEX = "^[A-Za-z]+",
+			ASSIGN_STR_REGEX = "=\\s*[^,]+", LINE_END_REGEX = ";$",
+			INIT_STR_REGEX = "(?:" + NAME_STR_REGEX + ")\\s*" + "(?:" + ASSIGN_STR_REGEX + ")?",
+			LINE_STR_REGEX = TYPE_STR_REGEX + "\\s+" + INIT_STR_REGEX + 
+			"\\s*(?:,\\s*" + INIT_STR_REGEX + ")*" + "\\s*" + LINE_END_REGEX;
+	private static final Pattern initPattern = Pattern.compile(INIT_STR_REGEX),
+			 					typePattern = Pattern.compile(TYPE_STR_REGEX),
+			 					linePattern = Pattern.compile(LINE_STR_REGEX);
+	private static final int TYPE_ARG = 0, NAME_ARG = 1, ASSIGN_ARG = 2, VALUE_ARG = 3, MIN_ARGS = 2;
+	private static final String[] validTypes = {"String", "int", "double", "boolean"};
+	private static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		String userInput;
+		System.out.println("String to search:");
+		while (!(userInput = scanner.nextLine()).equals("end")) {
+			foo(userInput);
+		}
+		
+	}
+	
+	private static void foo(String line) {
+		Matcher match = linePattern.matcher(line);
+		if (!match.find()) {
+			System.out.println("Not found");
+			return;
+		}
+		match = typePattern.matcher(line);
+		match.find();
+		System.out.println("Type: " + match.group());
+		line = line.substring(match.end(), line.length());
+		match = initPattern.matcher(line);
+		while (match.find()) {
+			System.out.println(match.group());
+			line = line.substring(match.end(), line.length());
+			match = initPattern.matcher(line);
+		}
+	}
+	
+	private static boolean isValidType(String aType) {
+		for (String type : validTypes) {
+			if (type.equals(aType))
+				return true;
+		}
+		return false;
+	}
+}
--- src/sjava/variables/Variable.java
+++ src/sjava/variables/Variable.java
@@ -6,64 +6,48 @@ import java.util.regex.*;
  * This class represents a data member in S Java files.
  * @param <T> The data-member's type.
  */
-public class Variable<T> {
-	T value;
-	String name;
+public class Variable {
+	String myName, myType;
+	Object myVal;
+	
 	boolean wasInit = false;
-	private static final String NAME_STR_REGEX = "(?:[A-Za-z]|_)\\w*";
-	private static final Pattern namePattern = Pattern.compile(NAME_STR_REGEX);
-	
-	private boolean isValidName(String aName) {
-		Matcher matcher = namePattern.matcher(aName);
-		return matcher.matches();
-	}
-	
-	/**
-	 * @param newName The name this data members will hold.
-	 * @throws BadVariableNameException 
-	 */
-	Variable(String newName) throws BadVariableNameException {
-		if (!isValidName(newName))
-			throw new BadVariableNameException(newName);
-		name = newName;
-	}
+	private static final String NAME_STR_REGEX = "(?:(?:[A-Za-z]\\w*)|(?:_\\w+))",
+			TYPE_STR_REGEX = "^[A-Za-z]+",
+			ASSIGN_STR_REGEX = "=\\s*[^,]+", LINE_END_REGEX = ";$",
+			INIT_STR_REGEX = "(?:" + NAME_STR_REGEX + ")\\s*" + "(?:" + ASSIGN_STR_REGEX + ")?",
+			LINE_STR_REGEX = TYPE_STR_REGEX + "\\s+" + INIT_STR_REGEX + 
+			"\\s*(?:,\\s*" + INIT_STR_REGEX + ")*" + "\\s*" + LINE_END_REGEX;
+	public static final Pattern initPattern = Pattern.compile(INIT_STR_REGEX),
+			 					typePattern = Pattern.compile(TYPE_STR_REGEX),
+			 					linePattern = Pattern.compile(LINE_STR_REGEX);
+	private static final int TYPE_ARG = 0, NAME_ARG = 1, ASSIGN_ARG = 2, VALUE_ARG = 3, MIN_ARGS = 2;
+	private static final String[] validTypes = {"String", "int", "double", "boolean"};
 	
 	/**
 	 * @param newName The name this data members will hold.
-	 * @param newVal The value it holds.
-	 * @throws BadVariableNameException 
+	 * @throws VariableException 
 	 */
-	Variable(String newName, T newVal) throws BadVariableNameException {
-		this(newName);
-		value = newVal;
-		wasInit = true;
+	public Variable(String variableLine) throws VariableException {
+	
+		
 	}
 	
-	/**
-	 * @return The value this data member holds
-	 */
-	T getValue() {
-		return value;
-	}
-	
-	/**
-	 * @param newVal Sets a new value to this data member
-	 */
-	void setValue(String newVal) {
-		try {
-			T val = (T) newVal;
-			value = val;
-		} catch (Exception e) {
-			
+	private static boolean isValidType(String aType) {
+		for (String type : validTypes) {
+			if (type.equals(aType))
+				return true;
 		}
-		wasInit = true;
+		return false;
 	}
 	
-	/**
-	 * @return True if the data-member was initialised, false otherwise.
-	 */
-	boolean isInitialized() {
-		return wasInit;
+	private static boolean isValidName(String aName) {
+		Matcher matcher = namePattern.matcher(aName);
+		return matcher.find();
+	}
+	
+	public static boolean isVariableInitLine(String line) {
+		Matcher match = linePattern.matcher(line);
+		return match.matches();
 	}
 	
 }
