28ff08eeb8c449da8de1b0e0ff22ba2844aae0b1
Evgeniya Dozortseva <evgeniyadozortseva@nwcse-guset-0979.cs.huji.ac.il>
2015-06-22 22:40:50 +0300

    checking the type of varible

--- src/oop/ex6/main/inner_scopes/methods/Method.java
+++ src/oop/ex6/main/inner_scopes/methods/Method.java
@@ -44,7 +44,7 @@ public class Method extends InnerScope {
 	}
 
 	/**
-	 * This method gets the name of the method object
+	 * Method gets the name of the method object
 	 * @return myName the name of the method
 	 */
 	public String getName() {
@@ -52,37 +52,46 @@ public class Method extends InnerScope {
 	}
 		
 	/**
-	 * 
+	 * Method gets the list of arguments of the method object
 	 */
 	public List<Variable> getArgs(){
 		return arguments;
 	}
 	
-	
+	/**
+	 * Method tries to read the given line as method call, throws exception if syntax is not legal
+	 * @param line - the line to be read as method call line
+	 * @param ancestor - the SJavaFile ancestor of the Scope, where method is called
+	 * @throws IllegalCodeException
+	 */
 	public static void handleMethodCall(String line, SJavaFile ancestor) throws IllegalCodeException {
 		String name; String[] args;
 		Matcher match = methodCallPattern.matcher(line);
 		if (!match.matches())
-			throw new IllegalMethodCallException(line);
+			throw new IllegalMethodCallException(line); // whole line is not a proper method call
 		
 		name = match.group(1); args = match.group(2).split("\\s*,\\s*");
+		//catches separately methods name and the parametres
 		String[] emptyString = {};
 		args = args[0].equals("") ? emptyString : args;
-		Method theMethod = ancestor.getMethod(name);
-		if (theMethod == null) {
+		Method theMethod = ancestor.getMethod(name);//looks for the method by its name in ancestors' methods
+		if (theMethod == null) { //the call was for method that wasn't defined
 			throw new IllegalMethodCallException(line);
 		}
 		
 		List<Variable> methodArgs = theMethod.getArgs();
-		if (methodArgs.size() != args.length)
+		if (methodArgs.size() != args.length) //not right amount of parametres is given
 			throw new IllegalMethodCallException(line);
 		
-		Variable v;
+		Variable v, otherVar;
 		for (int i = 0; i < methodArgs.size() ; i++) {
 			v = methodArgs.get(i);
-			if (SJavaFile.isExactMatch(v.getType().getValuePattern(), args[i])) {
+			if (SJavaFile.isExactMatch(v.getType().getValuePattern(), args[i])) { //parameter fits argument
 				continue;
-			} else if (theMethod.getVariable(args[i]) != null) {
+			} else if ( (otherVar = theMethod.getVariable(args[i])) != null) { //found variable
+				if(v.getType()!= otherVar.getType()){
+					throw new IllegalParamsException(line);
+				}
 				continue;
 			} else {
 				throw new IllegalMethodCallException(line);
